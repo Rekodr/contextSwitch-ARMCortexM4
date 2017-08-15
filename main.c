637e58e24d765fb4aa345d353ff3176a22449e19
Recodeo Rekod <REKODR@mail.gvsu.edu>
2017-08-15 15:53:43 -0400

    initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+
+/* DriverLib Includes */
+#include "driverlib.h"
+
+/* Standard Includes */
+#include <stdint.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    /* Stop Watchdog  */
+    MAP_WDT_A_holdTimer();
+
+    while(1)
+    {
+        
+    }
+}
